4bcbea30115d04506fd1ee729fe0870371d9be98
wautsns <wautsns@foxmail.com>
2019-11-06 21:43:57 +0800

    feat(core): 为 OkAuthManager 新增方法

--- okauth-core/src/main/java/com/github/wautsns/okauth/core/manager/OkAuthManager.java
+++ okauth-core/src/main/java/com/github/wautsns/okauth/core/manager/OkAuthManager.java
@@ -1,6 +1,7 @@
 package com.github.wautsns.okauth.core.manager;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.github.wautsns.okauth.core.client.core.OkAuthClient;
 import com.github.wautsns.okauth.core.client.core.OpenPlatform;
@@ -16,6 +17,15 @@ public class OkAuthManager {
 
     public OkAuthManager(Map<OpenPlatform, OkAuthClient> clients) {
         this.clients = clients;
+    }
+
+    public OkAuthClient getClient(String openPlatform) {
+        for (Entry<OpenPlatform, OkAuthClient> entry : clients.entrySet()) {
+            if (entry.getKey().getIdentifier().equalsIgnoreCase(openPlatform)) {
+                return entry.getValue();
+            }
+        }
+        return getClient((OpenPlatform) null);
     }
 
     public OkAuthClient getClient(OpenPlatform openPlatform) {
