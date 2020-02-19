3362a32cf914b16100f2ef8a2e81568e9395ff5d
wautsns <wautsns@foxmail.com>
2020-02-19 17:52:17 +0800

    refactor(~core): 为 OAuthUser 新增获取 OpenPlatform 的方法

--- okauth-core/src/main/java/com/github/wautsns/okauth/core/client/builtin/github/GitHubUser.java
+++ okauth-core/src/main/java/com/github/wautsns/okauth/core/client/builtin/github/GitHubUser.java
@@ -18,6 +18,7 @@ package com.github.wautsns.okauth.core.client.builtin.github;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.github.wautsns.okauth.core.client.builtin.BuiltInOpenPlatform;
+import com.github.wautsns.okauth.core.client.core.OpenPlatform;
 import com.github.wautsns.okauth.core.client.core.dto.OAuthUser;
 import com.github.wautsns.okauth.core.client.util.http.Response;
 
@@ -40,8 +41,8 @@ public class GitHubUser extends OAuthUser {
     }
 
     @Override
-    public String getOpenPlatformIdentifier() {
-        return BuiltInOpenPlatform.GITHUB.getIdentifier();
+    public OpenPlatform getOpenPlatform() {
+        return BuiltInOpenPlatform.GITHUB;
     }
 
     @Override
--- okauth-core/src/main/java/com/github/wautsns/okauth/core/client/builtin/oschina/OSChinaUser.java
+++ okauth-core/src/main/java/com/github/wautsns/okauth/core/client/builtin/oschina/OSChinaUser.java
@@ -18,6 +18,7 @@ package com.github.wautsns.okauth.core.client.builtin.oschina;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.github.wautsns.okauth.core.client.builtin.BuiltInOpenPlatform;
+import com.github.wautsns.okauth.core.client.core.OpenPlatform;
 import com.github.wautsns.okauth.core.client.core.dto.OAuthUser;
 import com.github.wautsns.okauth.core.client.util.http.Response;
 
@@ -40,8 +41,8 @@ public class OSChinaUser extends OAuthUser {
     }
 
     @Override
-    public String getOpenPlatformIdentifier() {
-        return BuiltInOpenPlatform.OSCHINA.getIdentifier();
+    public OpenPlatform getOpenPlatform() {
+        return BuiltInOpenPlatform.OSCHINA;
     }
 
     @Override
